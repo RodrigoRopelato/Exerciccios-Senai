5a67dfa1515f5f02a02da44af98fd3211871eb56
Rodrigo Ropelato <rodrigoropelato@hotmail.com>
2021-09-12 23:05:44 -0300

    exercicios senai

---
+++ Exercicios/Form1.Designer.cs
@@ -0,0 +1,418 @@
+﻿
+namespace Exercicios
+{
+    partial class Exercicios
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btn_Ex1_Converter = new System.Windows.Forms.Button();
+            this.txt_C = new System.Windows.Forms.TextBox();
+            this.txt_F = new System.Windows.Forms.TextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txt_A = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.txt_L = new System.Windows.Forms.TextBox();
+            this.txt_Comp = new System.Windows.Forms.TextBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txt_consumo = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.btn_MediaConsumo = new System.Windows.Forms.Button();
+            this.txt_Km_fim = new System.Windows.Forms.TextBox();
+            this.txt_Km_inicio = new System.Windows.Forms.TextBox();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.btn_Inverter = new System.Windows.Forms.Button();
+            this.btnPalavras = new System.Windows.Forms.Button();
+            this.label10 = new System.Windows.Forms.Label();
+            this.btnTamanho = new System.Windows.Forms.Button();
+            this.txt_Saida = new System.Windows.Forms.TextBox();
+            this.txt_Entrada = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.groupBox4.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.btn_Ex1_Converter);
+            this.groupBox1.Controls.Add(this.txt_C);
+            this.groupBox1.Controls.Add(this.txt_F);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(200, 100);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Exercicio1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 56);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 15);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Celsius";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(63, 15);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Fahrenheit";
+            // 
+            // btn_Ex1_Converter
+            // 
+            this.btn_Ex1_Converter.Location = new System.Drawing.Point(112, 42);
+            this.btn_Ex1_Converter.Name = "btn_Ex1_Converter";
+            this.btn_Ex1_Converter.Size = new System.Drawing.Size(75, 23);
+            this.btn_Ex1_Converter.TabIndex = 2;
+            this.btn_Ex1_Converter.Text = "Converter";
+            this.btn_Ex1_Converter.UseVisualStyleBackColor = true;
+            this.btn_Ex1_Converter.Click += new System.EventHandler(this.btn_Ex1_Converter_Click);
+            // 
+            // txt_C
+            // 
+            this.txt_C.Location = new System.Drawing.Point(6, 71);
+            this.txt_C.Name = "txt_C";
+            this.txt_C.Size = new System.Drawing.Size(85, 23);
+            this.txt_C.TabIndex = 1;
+            this.txt_C.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txt_C_KeyPress);
+            // 
+            // txt_F
+            // 
+            this.txt_F.Location = new System.Drawing.Point(6, 33);
+            this.txt_F.Name = "txt_F";
+            this.txt_F.Size = new System.Drawing.Size(85, 23);
+            this.txt_F.TabIndex = 0;
+            this.txt_F.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txt_F_KeyPress);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Controls.Add(this.txt_A);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.button1);
+            this.groupBox2.Controls.Add(this.txt_L);
+            this.groupBox2.Controls.Add(this.txt_Comp);
+            this.groupBox2.Location = new System.Drawing.Point(12, 118);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(200, 147);
+            this.groupBox2.TabIndex = 5;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Exercicio 2";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(6, 97);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(39, 15);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "Altura";
+            // 
+            // txt_A
+            // 
+            this.txt_A.Location = new System.Drawing.Point(6, 112);
+            this.txt_A.Name = "txt_A";
+            this.txt_A.Size = new System.Drawing.Size(85, 23);
+            this.txt_A.TabIndex = 5;
+            this.txt_A.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txt_Comp_KeyPress);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 56);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(47, 15);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Largura";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(6, 18);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(82, 15);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Comprimento";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(112, 56);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 56);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Calcular Volume";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txt_L
+            // 
+            this.txt_L.Location = new System.Drawing.Point(6, 71);
+            this.txt_L.Name = "txt_L";
+            this.txt_L.Size = new System.Drawing.Size(85, 23);
+            this.txt_L.TabIndex = 1;
+            this.txt_L.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txt_Comp_KeyPress);
+            // 
+            // txt_Comp
+            // 
+            this.txt_Comp.Location = new System.Drawing.Point(6, 33);
+            this.txt_Comp.Name = "txt_Comp";
+            this.txt_Comp.Size = new System.Drawing.Size(85, 23);
+            this.txt_Comp.TabIndex = 0;
+            this.txt_Comp.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txt_Comp_KeyPress);
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.label6);
+            this.groupBox3.Controls.Add(this.txt_consumo);
+            this.groupBox3.Controls.Add(this.label7);
+            this.groupBox3.Controls.Add(this.label8);
+            this.groupBox3.Controls.Add(this.btn_MediaConsumo);
+            this.groupBox3.Controls.Add(this.txt_Km_fim);
+            this.groupBox3.Controls.Add(this.txt_Km_inicio);
+            this.groupBox3.Location = new System.Drawing.Point(12, 271);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(200, 149);
+            this.groupBox3.TabIndex = 7;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Exercicio 3";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(6, 97);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(123, 15);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "Combustivel Percurso";
+            // 
+            // txt_consumo
+            // 
+            this.txt_consumo.Location = new System.Drawing.Point(6, 112);
+            this.txt_consumo.Name = "txt_consumo";
+            this.txt_consumo.Size = new System.Drawing.Size(85, 23);
+            this.txt_consumo.TabIndex = 5;
+            this.txt_consumo.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txt_Comp_KeyPress);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(6, 56);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(53, 15);
+            this.label7.TabIndex = 4;
+            this.label7.Text = "Km Final";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(6, 18);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(59, 15);
+            this.label8.TabIndex = 3;
+            this.label8.Text = "Km Inicial";
+            // 
+            // btn_MediaConsumo
+            // 
+            this.btn_MediaConsumo.Location = new System.Drawing.Point(112, 38);
+            this.btn_MediaConsumo.Name = "btn_MediaConsumo";
+            this.btn_MediaConsumo.Size = new System.Drawing.Size(75, 56);
+            this.btn_MediaConsumo.TabIndex = 2;
+            this.btn_MediaConsumo.Text = "Calcular Média Consumo";
+            this.btn_MediaConsumo.UseVisualStyleBackColor = true;
+            this.btn_MediaConsumo.Click += new System.EventHandler(this.btn_MediaConsumo_Click);
+            // 
+            // txt_Km_fim
+            // 
+            this.txt_Km_fim.Location = new System.Drawing.Point(6, 71);
+            this.txt_Km_fim.Name = "txt_Km_fim";
+            this.txt_Km_fim.Size = new System.Drawing.Size(85, 23);
+            this.txt_Km_fim.TabIndex = 1;
+            this.txt_Km_fim.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txt_Comp_KeyPress);
+            // 
+            // txt_Km_inicio
+            // 
+            this.txt_Km_inicio.Location = new System.Drawing.Point(6, 33);
+            this.txt_Km_inicio.Name = "txt_Km_inicio";
+            this.txt_Km_inicio.Size = new System.Drawing.Size(85, 23);
+            this.txt_Km_inicio.TabIndex = 0;
+            this.txt_Km_inicio.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txt_Km_inicio_KeyPress);
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.label9);
+            this.groupBox4.Controls.Add(this.btn_Inverter);
+            this.groupBox4.Controls.Add(this.btnPalavras);
+            this.groupBox4.Controls.Add(this.label10);
+            this.groupBox4.Controls.Add(this.btnTamanho);
+            this.groupBox4.Controls.Add(this.txt_Saida);
+            this.groupBox4.Controls.Add(this.txt_Entrada);
+            this.groupBox4.Location = new System.Drawing.Point(218, 12);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(287, 200);
+            this.groupBox4.TabIndex = 5;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "Exercicio 4";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(6, 120);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(66, 15);
+            this.label9.TabIndex = 6;
+            this.label9.Text = "Saida Texto";
+            // 
+            // btn_Inverter
+            // 
+            this.btn_Inverter.Location = new System.Drawing.Point(206, 92);
+            this.btn_Inverter.Name = "btn_Inverter";
+            this.btn_Inverter.Size = new System.Drawing.Size(75, 23);
+            this.btn_Inverter.TabIndex = 5;
+            this.btn_Inverter.Text = "Inverter";
+            this.btn_Inverter.UseVisualStyleBackColor = true;
+            this.btn_Inverter.Click += new System.EventHandler(this.btn_Inverter_Click);
+            // 
+            // btnPalavras
+            // 
+            this.btnPalavras.Location = new System.Drawing.Point(106, 92);
+            this.btnPalavras.Name = "btnPalavras";
+            this.btnPalavras.Size = new System.Drawing.Size(75, 23);
+            this.btnPalavras.TabIndex = 4;
+            this.btnPalavras.Text = "Palavras";
+            this.btnPalavras.UseVisualStyleBackColor = true;
+            this.btnPalavras.Click += new System.EventHandler(this.btnPalavras_Click);
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(6, 18);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(78, 15);
+            this.label10.TabIndex = 3;
+            this.label10.Text = "Entrada Texto";
+            // 
+            // btnTamanho
+            // 
+            this.btnTamanho.Location = new System.Drawing.Point(6, 92);
+            this.btnTamanho.Name = "btnTamanho";
+            this.btnTamanho.Size = new System.Drawing.Size(75, 23);
+            this.btnTamanho.TabIndex = 2;
+            this.btnTamanho.Text = "Tamanho";
+            this.btnTamanho.UseVisualStyleBackColor = true;
+            this.btnTamanho.Click += new System.EventHandler(this.btnTamanho_Click);
+            // 
+            // txt_Saida
+            // 
+            this.txt_Saida.Location = new System.Drawing.Point(6, 138);
+            this.txt_Saida.Multiline = true;
+            this.txt_Saida.Name = "txt_Saida";
+            this.txt_Saida.Size = new System.Drawing.Size(275, 56);
+            this.txt_Saida.TabIndex = 1;
+            this.txt_Saida.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txt_C_KeyPress);
+            // 
+            // txt_Entrada
+            // 
+            this.txt_Entrada.Location = new System.Drawing.Point(6, 33);
+            this.txt_Entrada.Multiline = true;
+            this.txt_Entrada.Name = "txt_Entrada";
+            this.txt_Entrada.Size = new System.Drawing.Size(275, 53);
+            this.txt_Entrada.TabIndex = 0;
+            // 
+            // Exercicios
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(514, 426);
+            this.Controls.Add(this.groupBox4);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Exercicios";
+            this.Text = "Exercicios";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.groupBox4.ResumeLayout(false);
+            this.groupBox4.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btn_Ex1_Converter;
+        private System.Windows.Forms.TextBox txt_C;
+        private System.Windows.Forms.TextBox txt_F;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txt_A;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txt_L;
+        private System.Windows.Forms.TextBox txt_Comp;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txt_consumo;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Button btn_MediaConsumo;
+        private System.Windows.Forms.TextBox txt_Km_fim;
+        private System.Windows.Forms.TextBox txt_Km_inicio;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Button btn_Inverter;
+        private System.Windows.Forms.Button btnPalavras;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Button btnTamanho;
+        private System.Windows.Forms.TextBox txt_Saida;
+        private System.Windows.Forms.TextBox txt_Entrada;
+    }
+}
+
---
+++ Exercicios/Form1.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+
+namespace Exercicios
+{
+    public partial class Exercicios : Form
+    {
+        public Exercicios()
+        {
+            InitializeComponent();
+        }
+
+        //Exercicio 1
+        private void btn_Ex1_Converter_Click(object sender, EventArgs e)
+        {
+            txt_C.Text = ((double.Parse(txt_F.Text) - 32) * 5 / 9).ToString("0#.##");
+        }
+
+        //Exercicio 2
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (String.IsNullOrEmpty(txt_Comp.Text) || String.IsNullOrEmpty(txt_A.Text) || String.IsNullOrEmpty(txt_L.Text))
+                MessageBox.Show("insira um valor valido", "Atenção!");
+            else
+                if (double.Parse(txt_Comp.Text) > 0 && double.Parse(txt_A.Text) > 0 && double.Parse(txt_L.Text) > 0)
+                MessageBox.Show("O volume da caixa é " + (double.Parse(txt_Comp.Text) * double.Parse(txt_L.Text) * double.Parse(txt_A.Text)).ToString("##.##"), "Volume");
+            else
+                MessageBox.Show("insira um valor maior que zero", "Atenção!");
+        }
+
+        //Exercicio 3
+        private void btn_MediaConsumo_Click(object sender, EventArgs e)
+        {
+            if (String.IsNullOrEmpty(txt_Km_inicio.Text) || String.IsNullOrEmpty(txt_Km_fim.Text) || String.IsNullOrEmpty(txt_consumo.Text))
+                MessageBox.Show("insira um valor valido", "Atenção!");
+            else
+                if (double.Parse(txt_Km_fim.Text) > double.Parse(txt_Km_inicio.Text))
+                MessageBox.Show("A média de consumo foi " + ((double.Parse(txt_Km_fim.Text) - double.Parse(txt_Km_inicio.Text)) / double.Parse(txt_consumo.Text)).ToString("##.##") + " km por litro", "Consumo Médio");
+            else
+                MessageBox.Show("A Quilometragem inicial não pode ser maior que a final", "Atenção!");
+        }
+
+        //Exercicio 4
+        private void btnTamanho_Click(object sender, EventArgs e)
+        {
+            txt_Saida.Clear();
+            txt_Saida.Text = "O texto contém " + txt_Entrada.TextLength.ToString() + " caracteres.";
+        }
+        private void btnPalavras_Click(object sender, EventArgs e)
+        {
+            txt_Saida.Clear();
+            txt_Saida.Text = "O texto contém " + txt_Entrada.Text.Split(' ').Length.ToString() + " palavras";
+        }
+        private void btn_Inverter_Click(object sender, EventArgs e)
+        {
+            txt_Saida.Clear();
+            for (int i = txt_Entrada.Text.Length-1; i >= 0; i--)
+                txt_Saida.Text += txt_Entrada.Text[i];
+        }
+
+
+
+        #region Auxiliares do Exercicio 1
+        //faz com que o textBox do exercicio 1 só aceite numeros,  ponto ,sinal - e backspace
+        private void txt_F_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            //Se a tecla digitada não for número e nem backspace
+            if (!char.IsDigit(e.KeyChar) && e.KeyChar != 08 && e.KeyChar != 46 && e.KeyChar != 45)
+            {
+                //Atribui True no Handled para cancelar o evento
+                e.Handled = true;
+                //MessageBox.Show("apenas numeros são permitidos no campo", "Atenção!");
+            }
+        }
+        //faz com que o textBox do exercicio 1 só aceite a tecla backspace
+        private void txt_C_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar != 08)
+            {
+                //Atribui True no Handled para cancelar o evento
+                e.Handled = true;
+            }
+        }
+
+        #endregion
+
+        #region Auxiliares do Exercicio 2 
+        //faz com que o textBox do exercicio 2 só aceite numeros,  ponto e backspace
+        private void txt_Comp_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            //Se a tecla digitada não for número e nem backspace
+            if (!char.IsDigit(e.KeyChar) && e.KeyChar != 08 && e.KeyChar != 46)
+            {
+                //Atribui True no Handled para cancelar o evento
+                e.Handled = true;
+            }
+        }
+        #endregion
+
+        #region Auxiliares do Exercicio 3
+        //faz com que o textBox do exercicio 3 só aceite numeros e backspace
+        private void txt_Km_inicio_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            //Se a tecla digitada não for número e nem backspace
+            if (!char.IsDigit(e.KeyChar) && e.KeyChar != 08)
+            {
+                //Atribui True no Handled para cancelar o evento
+                e.Handled = true;
+            }
+        }
+
+
+
+        #endregion
+
+
+    }
+}
